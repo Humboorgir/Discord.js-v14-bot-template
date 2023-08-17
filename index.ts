6bed292d89ec62db48cbb91da365cdc47e2fd7e0
Humboorgir <hardcraftap@gmail.com>
2023-08-17 23:58:15 +0330

    Write some code to get the bot running

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
 require("dotenv").config();
+
+import type { Client } from "discord.js";
+import * as Discord from "discord.js";
+const { Guilds, GuildMessages, MessageContent } = Discord.GatewayIntentBits;
+
+const client = new Discord.Client({ intents: [Guilds, GuildMessages, MessageContent] });
+const { DISCORD_TOKEN } = process.env;
+
+client.on("ready", (client: Client) => {
+  console.log(`Logged in as ${client.user!.tag}`);
+});
+
+client.login(DISCORD_TOKEN);
