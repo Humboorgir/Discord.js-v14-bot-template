e2ad28e33b592a2eda7311396bb8310ae826477e
Humboorgir <hardcraftap@gmail.com>
2023-08-18 12:50:01 +0330

    Remove all commonjs imports/exports from the project

--- index.ts
+++ index.ts
@@ -1,51 +1,60 @@
 require("dotenv").config();
 
-import * as Discord from "discord.js";
+import { Client, Collection, GatewayIntentBits } from "discord.js";
 import * as path from "path";
 import * as fs from "fs";
 import type { Command } from "./types";
-const { Guilds, GuildMessages, MessageContent } = Discord.GatewayIntentBits;
+const { Guilds, GuildMessages, MessageContent } = GatewayIntentBits;
 
-const client = new Discord.Client({ intents: [Guilds, GuildMessages, MessageContent] });
+const client = new Client({ intents: [Guilds, GuildMessages, MessageContent] });
 const { DISCORD_TOKEN } = process.env;
 
-client.commands = new Discord.Collection<string, Command>();
+client.commands = new Collection<string, Command>();
+
+LoadEventHandler();
+
+LoadCommandHandler();
 
 // Event handler
-const eventsPath = path.join(__dirname, "events");
-const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith(".js") || file.endsWith(".ts"));
+async function LoadEventHandler() {
+  const eventsPath = path.join(__dirname, "events");
+  const eventFiles = fs
+    .readdirSync(eventsPath)
+    .filter((file) => file.endsWith(".js") || file.endsWith(".ts"));
 
-for (const file of eventFiles) {
-  const filePath = path.join(eventsPath, file);
-  const event = require(filePath).default;
-  console.log(`Loading event: ${event.name}`);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args));
+  for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const { default: event } = await import(filePath);
+    console.log(`Loading event: ${event.name}`);
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args));
+    } else {
+      client.on(event.name, (...args) => event.execute(...args));
+    }
   }
 }
 
 // Command handler
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
+async function LoadCommandHandler() {
+  const foldersPath = path.join(__dirname, "commands");
+  const commandFolders = fs.readdirSync(foldersPath);
+  for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs
+      .readdirSync(commandsPath)
+      .filter((file) => file.endsWith(".js") || file.endsWith(".ts"));
 
-for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js") || file.endsWith(".ts"));
-
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath).default;
-    if (!command.data || !command.execute) {
-      console.log(command.data);
-      console.log(`The command at ${filePath} is missing a required 'data' or 'execute' property.`);
-      continue;
+    for (const file of commandFiles) {
+      const filePath = path.join(commandsPath, file);
+      const { default: command } = await import(filePath);
+      if (!command.data || !command.execute) {
+        console.log(command.data);
+        console.log(`The command at ${filePath} is missing a required 'data' or 'execute' property.`);
+        continue;
+      }
+      console.log(`Loading command: ${command.data.name}`);
+      client.commands.set(command.data.name, command);
     }
-    console.log(`Loading command: ${command.data.name}`);
-    client.commands.set(command.data.name, command);
   }
 }
 
